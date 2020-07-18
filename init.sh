03cbd65af4c4ee87d014623c1d6af5a504c6c65f
Ivan Nemenchinskii <nemenivan@gmail.com>
2020-07-18 21:12:37 +0300

    email changed

--- init.sh
+++ init.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-dir=$HOME/dotfiles
+dotfiles=$HOME/dotfiles
 backup=$HOME/dotfiles.bak
 
 mkdir -p $backup
 
-mv $HOME/.gitconfig $backup/.gitconfig
-mv $HOME/.gitignore_global $backup/.gitignore_global
-mv $HOME/.vim $backup/.vim
-mv $HOME/.bashrc $backup/.bashrc
-mv $HOME/.zshrc $backup/.zshrc
-mv $HOME/.tmux.conf $backup/.tmux.conf
-mv $HOME/.oh-my-zsh $backup/.oh-my-zsh
-#for file in $dir/*
-#do
-  #filename=$(basename $file)
-  #echo $file
-  #if [[ "$filename" != "$(basename $0)" ]]; then
-    #echo "Creating link for .$filename"
-    ## Move existing dotfile to $backup
-    #mv $HOME/.$filename $backup/
-    ## Create symlink
-    #ln -sv $dir/$filename $HOME/.$filename
-  #fi
-#done
-ln -sv $dir/git/gitconfig $HOME/.gitconfig
-ln -sv $dir/git/gitignore_global $HOME/.gitignore_global
-ln -sv $dir/vim $HOME/.vim
-ln -sv $dir/bashrc $HOME/.bashrc
-ln -sv $dir/zshrc $HOME/.zshrc
-ln -sv $dir/tmux.conf $HOME/.tmux.conf
-ln -sv $dir/oh-my-zsh $HOME/.oh-my-zsh
+mv $HOME/.config/i3 $backup/i3
+ln -sv $dotfiles/i3 $HOME/.config/i3
+mv $HOME/.gitconfig $backup/gitconfig
+ln -sv $dotfiles/git/gitconfig $HOME/.gitconfig
 
+
+files=(vim bashrc tmux.conf)
+for fpath in ${files[@]}
+do
+    mv $HOME/.$fpath $backup/$fpath
+    ln -sv $dotfiles/$fpath $HOME/.$fpath
+done
